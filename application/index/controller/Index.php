cbe2d0a0fd1aba2fc4827ebf2033ef33394e66f6
yunxmc <2389640655@qq.com>
2017-08-21 23:03:37 +0800

    修复错误

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,8 +5,6 @@ use think\Controller;
 class Index extends Controller{
     public function index()
     {
-        $mc = new \Memcache();
-        $mc->connect('127.0.0.1', '11211');
         return $this->view->fetch();
     }
 
